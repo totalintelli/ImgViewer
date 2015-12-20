6ad13872c86c6148af4c821ae71099a6f3740563
totalintelli <kwanwoo5@naver.com>
2015-12-20 16:48:35 +0900

    frmViewer의 파일 보기 창에 컨트롤 동적 생성 및 컨트롤 배열 사용하기

--- frmViewer.cs
+++ frmViewer.cs
@@ -173,6 +173,96 @@ namespace ImgViewer
                 return false;
         }
 
+        // 픽처박스 컨트롤을 생성한다.
+        private void MakePicCtrl(int nIndex, String strFilePath)
+        {
+            PictureBox pic = new PictureBox();
+            Point pos;
+
+            pic.Name = "pic" + nIndex.ToString(); // 이름
+            pic.SizeMode = PictureBoxSizeMode.StretchImage;
+            pic.Tag = nIndex.ToString(); // tag에 컨트롤의 인덱스 저장
+            pic.Size = new Size(80, 80); // 크기
+
+            GetPos(nIndex, out pos);
+
+            pic.Location = pos;
+            pic.BorderStyle = BorderStyle.FixedSingle;
+            splitContainer1.Panel2.Controls.Add(pic); // 패널에 추가
+
+            try
+            {
+                pic.Image = System.Drawing.Bitmap.FromFile(strFilePath); // 그림 보여주기
+            }
+            catch
+            {
+                pic.Image = null;
+            }
+
+            // 클릭/더블클릭 이벤트와 연결한다.
+            pic.Click += new System.EventHandler(Ctrl_Click);
+            pic.DoubleClick += new System.EventHandler(Ctrl_DoubleClick);
+
+        }
+
+        // 라벨 컨트롤을 생성한다.
+        private void MakeLblCtrl(int nIndex, String strFileName)
+        {
+            Label lbl = new Label();
+            Point pos;
+
+            lbl.Name = "lbl" + nIndex.ToString(); // 이름
+            lbl.Tag = nIndex.ToString();
+            lbl.Size = new Size(80, 20); // 크기
+            lbl.TextAlign = ContentAlignment.MiddleLeft;
+
+            GetPos(nIndex, out pos);
+
+            lbl.Location = pos;
+            lbl.BorderStyle = BorderStyle.FixedSingle;
+            splitContainer1.Panel2.Controls.Add(lbl);
+            lbl.Text = strFileName; // 그림 이름 보여주기
+            lbl.BackColor = Color.GreenYellow;
+
+            // 클릭/더블클릭 이벤트와 연결한다.
+            lbl.Click += new System.EventHandler(Ctrl_Click);
+            lbl.DoubleClick += new System.EventHandler(Ctrl_DoubleClick);
+        }
+
+        private void GetPos(int nIndex, out Point pos)
+        {
+            pos = new Point();
+            Size sizePan = new Size(splitContainer1.Panel2.Width, splicContainer1.Panel2.Height);
+            int nXCnt;
+            int i;
+
+            if((nIndex % 2) == 0) //픽처박스 컨트롤 일 경우
+            {
+                nXCnt = (int)(sizePan.Width / 85);
+                if(nXCnt <= 0)
+                    return;
+
+                i = (int)(nIndex / 2) % nXCnt;
+                pos.X = (i * 80) + (5 * i) + 5;
+
+                i = (int)((nIndex / 2) / nXCnt);
+                pos.Y = (i * 80) + (35 * i) + 5;
+            }
+
+            else // 라벨 컨트롤일 경우
+            {
+                nXCnt = (int)(sizePan.Width / 85);
+
+                if(nXCnt <= 0)
+                    return;
+
+                i = (int)(nIndex / 2) % nXCnt;
+                pos.X = (i * 80) + (5 * i) + 5;
+
+                i = (int)((nIndex / 2) / nXCnt);
+                pos.Y = (i * 80) + (35 * i) + 5 + 82;
+            }
+        }
     }
             
 }
